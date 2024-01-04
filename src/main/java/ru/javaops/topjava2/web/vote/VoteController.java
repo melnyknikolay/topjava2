08eb84c7305b762cb0df5d5e70c6e4c833c58645
Mykola Melnyk <mmelnyk@playtika.com>
2024-01-04 22:41:56 +0200

    3 1 get votes

---
+++ src/main/java/ru/javaops/topjava2/web/vote/VoteController.java
@@ -0,0 +1,43 @@
+package ru.javaops.topjava2.web.vote;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.javaops.topjava2.model.Vote;
+import ru.javaops.topjava2.repository.VoteRepository;
+import ru.javaops.topjava2.web.AuthUser;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = VoteController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
+@Slf4j
+public class VoteController {
+    static final String REST_URL = "/api/votes";
+
+    private final VoteRepository repository;
+
+    @GetMapping
+    public List<Vote> getOwns() {
+        int authId = AuthUser.authId();
+        log.info("getOwns for userId={}", authId);
+        return repository.getByUserId(authId);
+    }
+
+    @GetMapping("/by-date")
+    public ResponseEntity<Vote> getOwnsByDate(@Nullable @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+        int authId = AuthUser.authId();
+        if (date == null) date = LocalDate.now();
+        log.info("getOwnsByDate for userId={} and {}", authId, date);
+        return ResponseEntity.of(repository.getByUserIdAndDate(authId, date));
+    }
+}
